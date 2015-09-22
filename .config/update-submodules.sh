2d3114031e89ac2eb41c73099b3dd95082925df1
kkwak <kkwak@outlook.com>
2015-09-22 21:44:03 +0100

    alias 3rdup to update all submodules

--- .config/update-submodules.sh
+++ .config/update-submodules.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
+FULLPATH_FILE="$(test -L "$0" && readlink "$0" || echo "$0")"
+SCRIPTPATH=$(dirname $FULLPATH_FILE)
+
+echo Updating dotfiles repo
+pushd $SCRIPTPATH
 git pull --recurse-submodules && git submodule update
+popd
 
 if [ -d "$HOME/.emacs.d" ]; then
   echo Updating Spacemacs Repo independently
