4e493458f471d7948025abec24da45cc75d4f8fd
kkwak <kkwak@outlook.com>
2016-05-14 10:31:18 +0100

    lein updates

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [
-                  [cider/cider-nrepl "0.9.1"]
-                  [lein-ancient "0.6.7"]
+                  [cider/cider-nrepl "0.11.0"]
+                  [lein-ancient "0.6.8"]
                  ]}}
